fe8038c66dbed936b55a0ee733a22b085363eded
zhangzhen <278203533@qq.com>
2021-03-08 14:48:23 +0800

    first commit

---
+++ Client.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace nebtden;
+
+class Client
+{
+    protected $a;
+
+    protected $b;
+
+    public function __construct(int $a, int $b)
+    {
+        $this->a = $a;
+        $this->b = $b;
+    }
+
+    public function addTogether()
+    {
+        return $this->a + $this->b;
+    }
+}
